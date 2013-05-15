c1387e9e94408d77293e38145c361ccdde8ef22b
FrankHB <frankhb1989@gmail.com>
2013-05-16 02:32:38 +0800

    EGE 库实现：移除 count_ptr 模板。
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- EGE/src/ege/head.h
+++ EGE/src/ege/head.h
@@ -12,63 +12,6 @@
 namespace ege
 {
 
-template<typename T>
-struct count_ptr
-{
-	explicit count_ptr(T* p)
-	{
-		m_cnt = new long(1);
-		m_p = p;
-	}
-	~count_ptr()
-	{
-		--*m_cnt;
-		if(*m_cnt == 0)
-		{
-			delete m_p;
-			m_p = static_cast<T*>(0);
-			delete m_cnt;
-			m_cnt = static_cast<long*>(0);
-		}
-	}
-	count_ptr(const count_ptr<T>& count_ptr_)
-	{
-		m_p = count_ptr_.m_p;
-		m_cnt = count_ptr_.m_cnt;
-		++*m_cnt;
-	}
-	count_ptr<T>& operator= (const count_ptr<T>& count_ptr_)
-	{
-		--*m_cnt;
-		if(*m_cnt == 0)
-		{
-			delete m_p;
-			m_p = static_cast<T*>(0);
-			delete m_cnt;
-			m_cnt = static_cast<long*>(0);
-		}
-		m_p = count_ptr_.m_p;
-		m_cnt = static_cast<long*>(count_ptr_.m_cnt);
-		++*m_cnt;
-		return *this;
-	}
-	operator T* () const
-	{
-		return m_p;
-	}
-	T& operator * () const
-	{
-		return *m_p;
-	}
-	T* operator -> () const
-	{
-		return m_p;
-	}
-private:
-	T* m_p;
-	long* m_cnt;
-};
-
 } // namespace ege
 
 #endif
