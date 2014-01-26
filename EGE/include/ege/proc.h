511548af748e6b630402430149910b25f075463b
FrankHB <frankhb1989@gmail.com>
2014-01-26 14:20:04 +0800

    EGE 库：移除输入回调类型。
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- EGE/include/ege/proc.h
+++ EGE/include/ege/proc.h
@@ -7,8 +7,6 @@ namespace ege
 {
 
 typedef void CALLBACK_PROC();
-typedef int __stdcall MSG_KEY_PROC(void*, unsigned, int);
-typedef int __stdcall MSG_MOUSE_PROC(void*, unsigned, int, int, int);
 
 EGEAPI int
 SetCloseHandler(CALLBACK_PROC* func);
