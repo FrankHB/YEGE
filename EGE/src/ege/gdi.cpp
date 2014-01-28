483dbc24f8759fc4ab558f4b303ca042a0238afa
FrankHB <frankhb1989@gmail.com>
2014-01-29 00:11:51 +0800

    EGE/src/ege/gdi.cpp: 添加空指针断言检查；整理格式。
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- EGE/src/ege/gdi.cpp
+++ EGE/src/ege/gdi.cpp
@@ -9,53 +9,72 @@ namespace ege
 int
 getimage(IMAGE* pDstImg, const char* pImgFile, int zoomWidth, int zoomHeight)
 {
+	yassume(pDstImg);
+
 	return pDstImg->getimage(pImgFile, zoomWidth, zoomHeight);
 }
 int
 getimage(IMAGE* pDstImg, const wchar_t* pImgFile, int zoomWidth, int zoomHeight)
 {
+	yassume(pDstImg);
+
 	return pDstImg->getimage(pImgFile, zoomWidth, zoomHeight);
 }
 int
 getimage(IMAGE* pDstImg, const char* pResType, const char* pResName,
 	int zoomWidth, int zoomHeight)
 {
+	yassume(pDstImg);
+
 	return pDstImg->getimage(pResType, pResName, zoomWidth, zoomHeight);
 }
 int
 getimage(IMAGE* pDstImg, const wchar_t* pResType, const wchar_t* pResName,
 	int zoomWidth, int zoomHeight)
 {
+	yassume(pDstImg);
+
 	return pDstImg->getimage(pResType, pResName, zoomWidth, zoomHeight);
 }
 
 void
 putimage(int dstX, int dstY, IMAGE* pSrcImg, ::DWORD dwRop)
 {
+	yassume(pSrcImg);
+
 	pSrcImg->putimage(dstX, dstY, dwRop);
 }
 void
 putimage(int dstX, int dstY, int dstWidth, int dstHeight, IMAGE* pSrcImg,
 	int srcX, int srcY, ::DWORD dwRop)
 {
+	yassume(pSrcImg);
+
 	pSrcImg->putimage(dstX, dstY, dstWidth, dstHeight, srcX, srcY, dwRop);
 }
 void
 putimage(IMAGE* pDstImg, int dstX, int dstY, IMAGE* pSrcImg, ::DWORD dwRop)
 {
+	yassume(pSrcImg);
+
 	pSrcImg->putimage(pDstImg, dstX, dstY, dwRop);
 }
 void
 putimage(IMAGE* pDstImg, int dstX, int dstY, int dstWidth, int dstHeight,
 	IMAGE* pSrcImg, int srcX, int srcY, ::DWORD dwRop)
 {
+	yassume(pSrcImg);
+
 	pSrcImg->putimage(pDstImg, dstX, dstY, dstWidth, dstHeight, srcX, srcY,
 		dwRop);
 }
 void
 putimage(IMAGE* pDstImg, int dstX, int dstY, int dstWidth, int dstHeight,
-	IMAGE* pSrcImg, int srcX, int srcY, int srcWidth, int srcHeight, ::DWORD dwRop)
+	IMAGE* pSrcImg, int srcX, int srcY, int srcWidth, int srcHeight,
+	::DWORD dwRop)
 {
+	yassume(pSrcImg);
+
 	pSrcImg->putimage(pDstImg, dstX, dstY, dstWidth, dstHeight, srcX, srcY,
 		srcWidth, srcHeight, dwRop);
 }
@@ -63,6 +82,8 @@ void
 putimage(int dstX, int dstY, int dstWidth, int dstHeight, IMAGE* pSrcImg,
 	int srcX, int srcY, int srcWidth, int srcHeight, ::DWORD dwRop)
 {
+	yassume(pSrcImg);
+
 	pSrcImg->putimage({}, dstX, dstY, dstWidth, dstHeight, srcX, srcY,
 		srcWidth, srcHeight, dwRop);
 }
@@ -71,78 +92,77 @@ putimage(int dstX, int dstY, int dstWidth, int dstHeight, IMAGE* pSrcImg,
 int
 saveimage(IMAGE* pimg, const char* filename)
 {
-	const auto img = CONVERT_IMAGE(pimg);
-
-	int ret = 0;
-
-	if(img)
-		ret = img->saveimage(filename);
-	return ret;
+	if(const auto img = CONVERT_IMAGE(pimg))
+		return img->saveimage(filename);
+	return 0;
 }
 
 int
 saveimage(IMAGE* pimg, const wchar_t* filename)
 {
-	const auto img = CONVERT_IMAGE(pimg);
-	int ret = 0;
-
-	if(img)
-		ret = img->saveimage(filename);
-	return ret;
+	if(const auto img = CONVERT_IMAGE(pimg))
+		return img->saveimage(filename);
+	return 0;
 }
 
 
 int
 savepng(IMAGE* pimg, const char* filename, int bAlpha)
 {
-	std::FILE* fp = std::fopen(filename, "wb");
+	if(std::FILE* fp = std::fopen(filename, "wb"))
+	{
+		yconstraint(pimg);
 
-	if(!fp)
-		return grFileNotFound;
+		const int ret(pimg->savepngimg(fp, bAlpha));
 
-	int ret = pimg->savepngimg(fp, bAlpha);
+		std::fclose(fp);
+		return ret;
+	}
+	return grFileNotFound;
 
-	std::fclose(fp);
-	return ret;
 }
 int
 savepng(IMAGE* pimg, const wchar_t* filename, int bAlpha)
 {
-	std::FILE* fp = _wfopen(filename, L"wb");
+	if(std::FILE* fp = _wfopen(filename, L"wb"))
+	{
+		yconstraint(pimg);
 
-	if(!fp)
-		return grFileNotFound;
+		const int ret(pimg->savepngimg(fp, bAlpha));
 
-	int ret = pimg->savepngimg(fp, bAlpha);
-
-	std::fclose(fp);
-	return ret;
+		std::fclose(fp);
+		return ret;
+	}
+	return grFileNotFound;
 }
 
 int
 getimage_pngfile(IMAGE* pimg, const char* filename)
 {
-	std::FILE* fp = std::fopen(filename, "rb");
+	if(std::FILE* fp = std::fopen(filename, "rb"))
+	{
+		yconstraint(pimg);
 
-	if(!fp)
-		return grFileNotFound;
+		const int ret(pimg->getpngimg(fp));
 
-	int ret = pimg->getpngimg(fp);
-
-	std::fclose(fp);
-	return ret;
+		std::fclose(fp);
+		return ret;
+	}
+	return grFileNotFound;
 }
 int
 getimage_pngfile(IMAGE* pimg, const wchar_t* filename)
 {
-	std::FILE* fp = _wfopen(filename, L"rb");
-	if(!fp)
-		return grFileNotFound;
+	if(std::FILE* fp = _wfopen(filename, L"rb"))
+	{
+		yconstraint(pimg);
 
-	int ret = pimg->getpngimg(fp);
+		const int ret(pimg->getpngimg(fp));
 
-	std::fclose(fp);
-	return ret;
+		std::fclose(fp);
+		return ret;
+	}
+	return grFileNotFound;
 }
 
 } // namespace ege;
