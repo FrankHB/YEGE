643514565e27aedbc6cd3a7e59cd83c13336daf1
FrankHB <frankhb1989@gmail.com>
2014-02-02 21:19:23 +0800

    EGE 库：明确函数 ege_sleep 和 delay 支持多线程环境下使用。
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- EGE/include/ege/time.h
+++ EGE/include/ege/time.h
@@ -8,12 +8,6 @@ namespace ege
 
 //时间函数（以下函数不能在多线程下使用，只能给绘图主线程调用）
 EGEAPI void
-ege_sleep(long ms);     // 至少延迟ms毫秒
-
-EGEAPI void
-delay(long ms);         // 至少延迟ms毫秒
-
-EGEAPI void
 delay_ms(long ms);      // 平均延迟ms毫秒
 
 EGEAPI void
@@ -23,6 +17,11 @@ EGEAPI void
 delay_jfps(double fps); // 平均延迟1000/fps毫秒，用于稳定逻辑帧率控制，绘图带跳帧
 
 // 以下函数可以多线程下使用，非图形(worker)线程的sleep使用这个
+EGEAPI void
+ege_sleep(long ms);     // 至少延迟ms毫秒
+
+EGEAPI void
+delay(long ms);         // 至少延迟ms毫秒
 
 EGEAPI void
 api_sleep(long dwMilliseconds);
