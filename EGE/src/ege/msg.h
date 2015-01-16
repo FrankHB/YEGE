5fa999f7f0a9eb9dab00eeb5494e5af771cf85d7
FrankHB <frankhb1989@gmail.com>
2015-01-16 20:25:58 +0800

    EGE/src/ege/msg.h: 修正未初始化的消息成员被读取。
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- EGE/src/ege/msg.h
+++ EGE/src/ege/msg.h
@@ -8,13 +8,20 @@ namespace ege
 
 struct EGEMSG
 {
-	::HWND        hwnd;
-	::UINT        message;
-	::WPARAM      wParam;
-	::LPARAM      lParam;
-	::DWORD       time;
-	::UINT        mousekey;
-	::UINT        flag;
+	::HWND hwnd = {};
+	::UINT message = 0;
+	::WPARAM wParam = 0;
+	::LPARAM lParam = 0;
+	::DWORD time = 0;
+	::UINT mousekey = 0;
+	::UINT flag = 0;
+
+	EGEMSG() = default;
+	EGEMSG(::HWND h, ::UINT m, ::WPARAM w, ::LPARAM l, ::DWORD t, ::UINT k,
+		::UINT f)
+		: hwnd(h), message(m), wParam(w), lParam(l), time(t), mousekey(k),
+		flag(f)
+	{}
 };
 
 } // namespace ege;
