cf2820348bc6d84eb1af7e972e7e698fbe35ae89
FrankHB <frankhb1989@gmail.com>
2015-01-20 19:25:36 +0800

    egejulia/egejulia.cpp: 使用标准库复数简化实现，并修复退出时的资源泄漏。
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- egejulia/egejulia.cpp
+++ egejulia/egejulia.cpp
@@ -5,108 +5,54 @@
 #include <ctime>
 #include <cmath>
 #include <cstring>
+#include <complex>
 
-// 定义常量
-#define MAXCOLOR 64     // 颜色数
-#define BF_W 1200
-#define BF_H 1200
+using std::size_t;
+using COMPLEX = std::complex<double>;
 
-int g_w, g_h;
-
-
-// 定义复数及乘、加运算
-
-
-// 定义复数
-struct COMPLEX
+template<typename _type, size_t _vBase>
+struct log_base
 {
-	double re;
-	double im;
+	static yconstexpr const _type value = log(_type(_vBase));
 };
 
-// 定义复数“乘”运算
-COMPLEX operator * (COMPLEX a, COMPLEX b)
+template<typename _type, size_t _vBase>
+yconstfn _type
+log_n(_type d)
 {
-	COMPLEX c;
-	c.re = a.re * b.re - a.im * b.im;
-	c.im = a.im * b.re + a.re * b.im;
-	return c;
+	return log(d) / log_base<_type, _vBase>::value;
 }
 
-// 定义复数“加”运算
-COMPLEX operator + (COMPLEX a, COMPLEX b)
-{
-	COMPLEX c;
-	c.re = a.re + b.re;
-	c.im = a.im + b.im;
-	return c;
-}
-
-
-
-// 定义颜色及初始化颜色
-
-
-// 定义颜色
-int Color[MAXCOLOR];
-
-// 初始化颜色
-void InitColor()
-{
-	// 使用 HSL 颜色模式产生角度 h1 到 h2 的渐变色
-	int h1 = 240, h2 = 30;
-	for(int i = 0; i < MAXCOLOR / 2; i++)
-	{
-		Color[i] = HSLtoRGB((float)h1, 1.0f, i * 2.0f / MAXCOLOR);
-		Color[MAXCOLOR - 1 - i] = HSLtoRGB((float)h2, 1.0f, i * 2.0f / MAXCOLOR);
-	}
-}
-
-double bilout = 256;
-double logmap[(18 * 18) * 32];
-double col_ins = 1.0 / bilout / bilout * 256 * 32;
-double col_r = 28, col_g = 16, col_b = 32;
-double col_ar = 0, col_ag = 0, col_ab = 128;
-
-const double gc_lb = log(2.0);
-double log2(double d)
-{
-	return log(d) / gc_lb;
-}
-
-void InitLog()
-{
-	for(int i = 0; i < (18 * 18) * 32; ++i)
-	{
-		double r = i / col_ins;
-		logmap[i] = 1 - log2(log2(r) / 2);
-	}
-}
-
-int iterToColor(double iter)
-{
-	return (int)(fabs(fmod(iter + 255, 510) - 255));
-}
-
-COLORREF colorMap(COMPLEX z, int iteration)
-{
-	double r = z.re * z.re + z.im * z.im;
-	r = iteration + logmap[(int)(r * col_ins)];
-	return RGB(iterToColor(r * col_r + col_ar), iterToColor(r * col_g + col_ag), iterToColor(r * col_b + col_ab));
-}
-
-int g_mi[BF_H][BF_W];
-
+yconstexpr size_t BF_W(1200), BF_H(1200);
 
 struct state
 {
 	int iter;
-	int ed;
+	bool ed;
 	COMPLEX z;
 	COMPLEX c;
-};
 
-state(*pMap)[BF_W] = new state[BF_H][BF_W];
+	void
+	MandelbrotEx()
+	{
+		if(iter < 64)
+		{
+			int k = 64, b = k;
+
+			while(k > 0)
+			{
+				--k;
+				z = z * z + c;
+				if(std::norm(z) > 4.0)
+				{
+					ed = true;
+					break;
+				}
+			}
+			iter += b - k;
+		}
+	}
+};
 
 struct updatelist
 {
@@ -114,275 +60,256 @@ struct updatelist
 	::POINT m_list[2][1920 * 1920];
 	int nBeg, nLen;
 	int nLen_n;
+
 	updatelist()
 	{
 		clear();
 	}
-	void clear()
+
+	void
+	clear()
 	{
 		p = m_list[0];
 		pn = m_list[1];
 		nBeg = nLen = nLen_n = 0;
 	}
-	void push(int x, int y)
+
+	void
+	push(int x, int y)
 	{
-		pn[nLen_n].x = x;
-		pn[nLen_n].y = y;
+		pn[nLen_n].x = x, pn[nLen_n].y = y;
 		++nLen_n;
 	}
-	int pop(int* x, int* y)
+
+	int
+	pop(int* x, int* y)
 	{
 		if(nBeg == nLen) return 0;
-		*x = p[nBeg].x;
-		*y = p[nBeg].y;
+		*x = p[nBeg].x, *y = p[nBeg].y;
 		++nBeg;
 		return 1;
 	}
-	void swap()
+
+	void
+	flip() ynothrow
 	{
 		nBeg = 0;
 		nLen = nLen_n;
 		nLen_n = 0;
-		::POINT* _p = p;
-		p = pn;
-		pn = _p;
+		std::swap(p, pn);
 	}
-}* g_pudlist = new updatelist;
-updatelist& g_udlist = *g_pudlist;
-state* g_st = {};
+};
 
 
-void addpoint(int x, int y, int = -1)
+class gstate
 {
-	if(x < 0 || x >= BF_W || y < 0 || y >= BF_H) return;
-	if(pMap[y][x].ed == 0)
+private:
+	const double bilout = 256;
+	const double col_ins = 1.0 / bilout / bilout * 256 * 32;
+	double logmap[(18 * 18) * 32];
+	size_t width, height;
+	unique_ptr<state[]> states;
+	state* g_st;
+	std::array<double, 6> colors{{28, 16, 32, 0, 0, 128}};
+	double sr, cr;
+
+public:
+	int g_mi[BF_H][BF_W];
+	state pgmap[BF_H][BF_W];
+	updatelist udlist;
+
+	gstate()
+		: width(getwidth({})), height(getheight({})),
+		states(new state[width * height]), g_st(&states[0])
 	{
-		g_udlist.push(x, y);
+		for(int i = 0; i < (18 * 18) * 32; ++i)
+			logmap[i] = 1 - log_n<double, 2>(log_n<double, 2>(i / col_ins) / 2);
 	}
-}
 
-void jaddpoint(int x, int y, int = -1)
-{
-	if(x < 0 || x >= g_w || y < 0 || y >= g_h) return;
-	if(g_st[y * g_w + x].ed == 0)
+	double
+	GetRatio() const ynothrow
 	{
-		g_udlist.push(x, y);
+		return width / double(height);
 	}
-}
 
-int MandelbrotEx(state& z)
-{
-	if(z.iter >= 64) return z.iter;
-	int k = 64;
-	int b = k;
-	while(k > 0)
+	void
+	init_states()
 	{
-		--k;
-		z.z = z.z * z.z + z.c;
-		if(z.z.re * z.z.re + z.z.im * z.z.im > 4.0)
-		{
-			z.ed = 1;
-			break;
-		}
+		double dc = 64, dca = 128, db = 16;
+
+		colors = {randomf() * dc + db, randomf() * dc + db, randomf() * dc + db,
+			randomf() * dca, randomf() * dca, randomf() * dca};
+
+		double rotate = randomf() * 3.14169265359 * 2;
+
+		sr = sin(rotate), cr = cos(rotate);
+		std::memset(g_st, 0, width * height * sizeof(state));
 	}
-	z.iter += b - k;
-	return z.iter;
-}
 
-int kbmouhit()
-{
-	if(kbmsg()) return 1;
-	//return kbhit() || MouseHit();
-	return 0;
-}
-
-int MDraw(double fromx, double fromy, double tox, double toy)
-{
-	std::clock();
-
-	int ret = 0;
-
+	static int
+	iterToColor(double iter)
 	{
+		return int(std::fabs(fmod(iter + 255, 510) - 255));
+	}
+
+	void
+	loop4()
+	{
+		udlist.flip();
+		for(size_t y = 0; y < height; ++y)
+			for(size_t x = 0; x < width; ++x)
+				if(!g_st[y * width + x].ed)
+					udlist.push(x, y);
+		udlist.flip();
+	}
+
+	::COLORREF
+	colorMap(COMPLEX z, int iteration)
+	{
+		const double r(iteration + logmap[size_t(std::norm(z) * col_ins)]);
+
+		return RGB(iterToColor(r * colors[0] + colors[3]), iterToColor(r
+			* colors[1] + colors[5]), iterToColor(r * colors[2] + colors[4]));
+	}
+
+	void
+	addpoint(int x, int y)
+	{
+		if(x < 0 || x >= int(BF_W) || y < 0 || y >= int(BF_H))
+			return;
+		if(!pgmap[y][x].ed)
+			udlist.push(x, y);
+	}
+
+	int
+	MDraw(double fromx, double fromy, double tox, double toy)
+	{
+		std::clock();
+
+		int ret = 0;
+
 		int x, y;
-		while(g_udlist.pop(&x, &y))
+		while(udlist.pop(&x, &y))
 		{
-			state& p = pMap[y][x];
-			if(p.iter == 0 && p.ed == 0)
+			state& p = pgmap[y][x];
+			if(p.iter == 0 && !p.ed)
 			{
-				COMPLEX z, c;
-				c.re = fromx + (tox - fromx) * (x / (double)BF_W);
-				c.im = fromy + (toy - fromy) * (y / (double)BF_H);
-				z.re = z.im = 0.0;
-				p.c = c;
-				p.z = z;
+				p.c = {fromx + (tox - fromx) * (x / (double)BF_W),
+					fromy + (toy - fromy) * (y / (double)BF_H)};
+				p.z = 0;
 			}
-			if(p.ed == 0)
+			if(!p.ed)
 			{
-				int k;
-				k = MandelbrotEx(p);
+				p.MandelbrotEx();
 				if(p.ed)
 				{
-					ret++;
-					{
-						addpoint(x, y - 1, k);
-						addpoint(x, y + 1, k);
-						addpoint(x - 1, y, k);
-						addpoint(x + 1, y, k);
-					}
-					g_mi[y][x] = k;
-					/*
-					{
-					    COLORREF c = 0;
-					    c = colorMap(p.z, p.iter);
-					    putpixel(x, y, c);
-					}// */
+					++ret;
+					addpoint(x, y - 1);
+					addpoint(x, y + 1);
+					addpoint(x - 1, y);
+					addpoint(x + 1, y);
+					g_mi[y][x] = p.iter;
 				}
 				else
-				{
 					addpoint(x, y);
-				}
 			}
-			if(kbmouhit()) break;
+			if(kbmsg())
+				break;
 		}
+		udlist.flip();
+		return ret;
 	}
-	g_udlist.swap();
-	return ret;
-}
 
-int g_updatepoint = 0;
+	int g_updatepoint = 0;
 
-// 绘制 Julia Set
-int JDraw(COMPLEX c, double fromx, double fromy, double tox, double toy, double sr, double cr)
-{
-	int ret = 0;
-	state* st = g_st - 1;
-	clock_t tt = std::clock();
-	g_updatepoint = 0;
-	for(int y = 0; y < g_h; y++)
+	// 绘制 Julia Set
+	size_t
+	JDraw(COMPLEX c, double fromx, double fromy, double tox, double toy)
 	{
-		for(int x = 0; x < g_w; x++)
-		{
-			++st;
-			if(st->ed)
-			{
-				continue;
-			}
-			COMPLEX& z = st->z;
+		size_t ret(0);
+		state* st(&states[0]);
+		clock_t tt = std::clock();
 
-			if(st->iter == 0)
+		g_updatepoint = 0;
+		for(size_t y = 0; y < height; ++y)
+		{
+			for(size_t x = 0; x < width; ++x, ++st)
 			{
-				double re = fromx + (tox - fromx) * (x / (double)g_w);
-				double im = fromy + (toy - fromy) * (y / (double)g_h);
-				z.re = cr * re + sr * im;
-				z.im = sr * re - cr * im;
-			}
-			else
-			{
-				//z = st->z;
-			}
-			st->iter++;
-			{
-				z = z * z + c;
-				if(z.re * z.re + z.im * z.im > bilout)
+				if(st->ed)
+					continue;
+				COMPLEX& z = st->z;
+
+				if(st->iter == 0)
 				{
-					st->ed = 1;
+					const double re = fromx + (tox - fromx)
+						* (x / double(width)),
+						im = fromy + (toy - fromy) * (y / double(height));
+
+					z = {cr * re + sr * im, sr * re - cr * im};
 				}
+			//	else
+			//		z = st->z;
+				++st->iter;
+				z = z * z + c;
+				if(std::norm(z) > bilout)
+					st->ed = true;
+				++ret;
+				if(st->ed)
+				{
+					putpixel(x, y, colorMap(z, st->iter));
+					g_updatepoint += 1;
+				}
+				else if(st->iter == 1)
+					putpixel_f(x, y, 0);
 			}
-			++ret;
-			if(st->ed)
+			if(std::clock() - tt > 10)
 			{
-				COLORREF c = 0;
-				c = colorMap(z, st->iter);
-				putpixel(x, y, c);
-				g_updatepoint += 1;
-			}
-			else if(st->iter == 1)
-			{
-				COLORREF c = 0;
-				//c = colorMap(z, st->iter);
-				putpixel_f(x, y, c);
+				tt = std::clock();
+				if(kbmsg())
+					return -1;
 			}
 		}
-		if(std::clock() - tt > 10)
-		{
-			tt = std::clock();
-			if(kbmouhit())
-			{
-				return -1;
-			}
-		}
+		return ret;
 	}
-	return ret;
-}
 
-int JDrawA(COMPLEX c, double, double, double, double)
-{
-	std::clock();
-
-	int ret = 0;
-
-	g_updatepoint = 0;
-
-	state* st = g_st;
-
+	size_t
+	JDrawA(COMPLEX c)
 	{
+		std::clock();
+		g_updatepoint = 0;
+
+		size_t ret = 0;
 		int x, y;
-		while(g_udlist.pop(&x, &y))
+
+		while(udlist.pop(&x, &y))
 		{
-			state& p = st[y * g_w + x];
+			auto& p(states[y * width + x]);
 			if(p.ed == 0)
 			{
-				int k;
-				{
-					p.iter++;
-					k = p.iter;
-					COMPLEX& z = p.z;
-					z = z * z + c;
-					if(z.re * z.re + z.im * z.im > bilout)
-					{
-						p.ed = 1;
-					}
-				}
-				ret ++;
+				auto& z(p.z);
+
+				z = z * z + c;
+				if(std::norm(z) > bilout)
+					p.ed = 1;
+				++p.iter, ++ret;
 				if(p.ed)
 				{
-					COLORREF c = 0;
-					c = colorMap(p.z, k);
-					putpixel(x, y, c);
+					putpixel(x, y, colorMap(p.z, p.iter));
 					g_updatepoint += 1;
 				}
 				else
-				{
-					g_udlist.push(x, y);
-				}
+					udlist.push(x, y);
 			}
-			/*
-			if(std::clock() - tt > 10)
-			{
-				delay(1);
-				tt = std::clock();
-				if(0 && kbmouhit())
-				{
-					return -1;
-				}
-			}//*/
 		}
+		udlist.flip();
+		return ret;
 	}
-	g_udlist.swap();
-	return ret;
-}
+};
 
-void init_st(int x, int y)
+
+int
+main()
 {
-	std::memset(g_st, 0, x * y * sizeof(state));
-}
-
-
-// 主函数
-
-int main()
-{
-	// 初始化绘图窗口及颜色
 	setinitmode(0x005, 0, 0);
 	//initgraph(320, 240);
 	initgraph(-1, -1);
@@ -390,112 +317,70 @@ int main()
 	flushmouse();
 	while(kbhit())
 		getch();
-	//InitColor();
-	InitLog();
-	g_w = getwidth({});
-	g_h = getheight({});
-	g_st = (state*)malloc(g_w * g_h * sizeof(state));
 	COMPLEX c{0.262, 0.002}, z{0, 0};
-	double r = 1.5, d = g_w / (double)g_h, rotate = 0, sr = sin(rotate), cr = cos(rotate);
-	init_st(g_w, g_h);
 	int n_update = 0;
-	double ftime = fclock();
-	double dc = 64, dca = 128, db = 16;
-	col_r = randomf() * dc + db;
-	col_g = randomf() * dc + db;
-	col_b = randomf() * dc + db;
-	col_ar = randomf() * dca;
-	col_ag = randomf() * dca;
-	col_ab = randomf() * dca;
-	rotate = randomf() * 360;
-	sr = sin(rotate), cr = cos(rotate);
-	for(int loop = 1; kbmouhit() == 0; ++loop)
+	auto p_gs(make_unique<gstate>());
+	auto& gs(*p_gs);
+	double ftime = fclock(), r = 1.5, d = gs.GetRatio();
+
+	gs.init_states();
+	for(int loop = 1; kbmsg() == 0; ++loop)
 	{
 		int ret;
 
 		if(loop <= 4)
 		{
-			ret = JDraw(c, z.re - r * d, z.im - r, z.re + r * d, z.im + r, sr, cr);
+			ret = gs.JDraw(c, z.real() - r * d, z.imag() - r, z.real() + r * d,
+				z.imag() + r);
 			if(loop == 4)
-			{
-				g_udlist.swap();
-				for(int y = 0; y < g_h; y++)
-					for(int x = 0; x < g_w; x++)
-						if(g_st[y * g_w + x].ed == 0)
-							g_udlist.push(x, y);
-				g_udlist.swap();
-			}
+				gs.loop4();
 		}
 		else
 		{
-			static int t = 0;
+			static int t;
 
-			ret = JDrawA(c, z.re - r * d, z.im - r, z.re + r * d, z.im + r);
+			ret = gs.JDrawA(c);
 			if(std::clock() - t > 30)
 			{
 				delay(1);
 				t = std::clock();
 			}
 		}
-		if(g_updatepoint == 0)
+		if(gs.g_updatepoint == 0)
 			++n_update;
 		else
 			n_update = 0;
-		if(0)
-		{
-			char str[500];
-
-			std::sprintf(str, "%d %d %f %f", g_w, g_h, r, d);
-			outtextxy(0, 0, str);
-		}
 		if(ret == 0 || n_update > 8 || loop > 1000)
 		{
 			loop = 0;
 
-			if(g_mi[0][0] == 0)
+			if(gs.g_mi[0][0] == 0)
 			{
 				delay(1);
-				std::memset(pMap, 0, BF_W * BF_H * sizeof(state));
-				g_udlist.clear();
-				for(int i = 0; i < BF_W; ++i)
+				std::memset(gs.pgmap, 0, BF_W * BF_H * sizeof(state));
+				gs.udlist.clear();
+				for(size_t i(0); i < BF_W; ++i)
 				{
-					addpoint(i, 0);
-					addpoint(i, BF_H - 1);
+					gs.addpoint(i, 0);
+					gs.addpoint(i, BF_H - 1);
 				}
-				for(int i = 0; i < 4;)
+				for(size_t i(0); i < 4; )
 				{
-					if(MDraw(-1.9, -1.2, 0.5, 1.2) == 0)
+					if(gs.MDraw(-1.9, -1.2, 0.5, 1.2) == 0)
 						++i;
 					else
 						i = 0;
-					if(kbmouhit())
+					if(kbmsg())
 						return 0;
 				}
 			}
-
-			double dc = 64, dca = 128, db = 16;
-
-			col_r = randomf() * dc + db;
-			col_g = randomf() * dc + db;
-			col_b = randomf() * dc + db;
-			col_ar = randomf() * dca;
-			col_ag = randomf() * dca;
-			col_ab = randomf() * dca;
-			rotate = randomf() * 360;
-			sr = sin(rotate), cr = cos(rotate);
+			gs.init_states();
 			do
 			{
-				c.re = randomf() * 2.4 - 1.9;
-				c.im = randomf() * 2.4 - 1.2;
-				int ir = (int)((c.re - (-1.9)) / (0.5 - (-1.9)) * BF_W);
-				int im = (int)((c.im - (-1.2)) / (1.2 - (-1.2)) * BF_H);
-				if(g_mi[im][ir] >= 16)
-				{
-					break;
-				}
+				c = {randomf() * 2.4 - 1.9, randomf() * 2.4 - 1.2};
 			}
-			while(1);
-			init_st(g_w, g_h);
+			while(gs.g_mi[(int)((c.imag() - (-1.2)) / (1.2 - (-1.2)) * BF_H)]
+				[(int)((c.real() - (-1.9)) / (0.5 - (-1.9)) * BF_W)] < 16);
 			n_update = 0;
 			if(fclock() - ftime < 3)
 				delay_ms((int)((3 - (fclock() - ftime)) * 1000));
