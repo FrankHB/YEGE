bb23de966e7aa7af18ae0d2adacfd9d4e0b3d042
FrankHB <frankhb1989@gmail.com>
2015-03-18 06:22:58 +0800

    EGE/src/ege/random.cpp: 简化实现。
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- EGE/src/ege/random.cpp
+++ EGE/src/ege/random.cpp
@@ -9,37 +9,26 @@ namespace
 {
 
 std::random_device rd;
-
-std::mt19937&
-get_mt()
-{
-	static std::mt19937 mt(rd());
-
-	return mt;
-}
+std::mt19937 mt(rd());
 
 } // unnamed namespace;
 
 void
 randomize()
 {
-	get_mt() = std::mt19937(rd());
+	mt = std::mt19937(rd());
 }
 
 unsigned
 random(unsigned n)
 {
-	std::uniform_int_distribution<unsigned> d(0, n);
-
-	return d(get_mt());
+	return std::uniform_int_distribution<unsigned>(0, n)(mt);
 }
 
 double
 randomf()
 {
-	std::uniform_real_distribution<float> d;
-
-	return d(get_mt());
+	return std::uniform_real_distribution<float>()(mt);
 }
 
 } // namespace ege;
