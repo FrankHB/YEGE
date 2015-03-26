0d78d9c70abd0f8501e6d387e860ba12ede25aea
FrankHB <frankhb1989@gmail.com>
2015-03-26 15:21:03 +0800

    egeball/egeball.cpp: 简化实现。
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- egeball/egeball.cpp
+++ egeball/egeball.cpp
@@ -2,128 +2,125 @@
 #include <graphics.h>
 #include <ege/fps.h>
 #include <cmath>
+#include <vector>
+#include <algorithm>
+
+template<typename _type>
+_type
+square(_type x)
+{
+	return x * x;
+}
+
+template<typename _type>
+_type
+dist(_type x1, _type y1, _type x2, _type y2)
+{
+	return square(x1 - x2) + square(y1 - y2);
+}
 
 struct Obj
 {
 	float x, y;
-	float vx, vy;
-	int r;
+	float vx = 0.F, vy = 0.F;
+	unsigned r;
+	float m;
 	int color;
+
+	Obj()
+		: color(
+		EGEAGRAY(0xFF, hsv2rgb(random(10000) * 360.F / 10000, 1.F, 1.F)))
+	{}
+
+	void
+	init_xyr()
+	{
+		do{
+			x = random(getwidth());
+			y = random(getheight());
+			r = random(40) + 20;
+		}while(x < r || x >= 640 - r || y < r || y >= 480 - r);
+		m = square(r);
+	}
+
+	void
+	draw() const
+	{
+		setfillcolor(color);
+		ege_fillellipse(x - r, y - r, r * 2, r * 2);
+	}
+
+	void
+	update()
+	{
+		vy += 0.05F;
+		x += vx;
+		y += vy;
+		if(y >= 480 - r && vy > 0)
+		{
+			y -= vy;
+			vy = -vy;
+		}
+		if(x < r && vx < 0)
+			vx = -vx;
+		if(x >= 640 - r && vx > 0)
+			vx = -vx;
+	}
 };
 
-
-//定义一个AniObj类
-class AniObj
+bool
+crash(const Obj& a, const Obj& b)
 {
-private:
-	Obj obj[20];
-	int n = 8;
+	return dist(a.x, a.y, b.x, b.y) < square(a.r + b.r);
+}
 
-public:
-	AniObj()
+void
+check_crash(Obj& a, Obj& b)
+{
+	const float mdiff(a.m - b.m), msum(a.m + b.m);
+	const float sx(a.x - b.x), sy(a.y - b.y), hysxy(hypot(sx, sy)),
+		s1x(sx / hysxy), s1y(sy / hysxy), t1x(-s1y), t1y(s1x),
+		vas(a.vx * s1x + a.vy * s1y), vat(a.vx * t1x + a.vy * t1y),
+		vbs(b.vx * s1x + b.vy * s1y), vbt(b.vx * t1x + b.vy * t1y),
+		vasf((2 * b.m * vbs + vas * mdiff) / msum),
+		vbsf((2 * a.m * vas - vbs * mdiff) / msum);
+
+	a.vx = vasf * s1x + vat * t1x;
+	a.vy = vasf * s1y + vat * t1y;
+	b.vx = vbsf * s1x + vbt * t1x;
+	b.vy = vbsf * s1y + vbt * t1y;
+}
+
+struct AniObj
+{
+	std::vector<Obj> objs;
+
+	AniObj(size_t num)
+		: objs(num)
 	{
-		for(int i = 0; i < n; i++)
-		{
-			bool goon;
-
+		for(auto& a : objs)
 			do
 			{
-				goon = {};
-				obj[i].x = random(getwidth());
-				obj[i].y = random(getheight());
-				obj[i].r = random(40) + 20;
-				if(obj[i].x < obj[i].r || obj[i].x >= 640 - obj[i].r
-					|| obj[i].y < obj[i].r || obj[i].y >= 480 - obj[i].r)
-					goon = true;
-				else if(i != 0)
-					for(int j = i - 1; j >= 0; j--)
-						if(IsCrash(obj[i], obj[j]))
-						{
-							goon = true;
-							break;
-						}
-			}while(goon);
-			obj[i].vx = obj[i].vy = 0.F;
-			obj[i].color = EGEAGRAY(0xFF,
-				hsv2rgb(random(10000) * 360.F / 10000, 1.F, 1.F));
-		}
-	}
-
-	//更新位置等相关属性
-	void
-	updateobj()
-	{
-		int i, j;
-		for(i = 0; i < n; i++)
-		{
-			obj[i].vy += 0.05f;
-			obj[i].x += obj[i].vx;
-			obj[i].y += obj[i].vy;
-			if(obj[i].y >= 480 - obj[i].r && obj[i].vy > 0)
-			{
-				obj[i].y -= obj[i].vy;
-				obj[i].vy = - obj[i].vy;
-			}
-			if(obj[i].x < obj[i].r && obj[i].vx < 0)
-				obj[i].vx = - obj[i].vx;
-			if(obj[i].x >= 640 - obj[i].r && obj[i].vx > 0)
-				obj[i].vx = - obj[i].vx;
-		}
-		for(i = 1; i < n; i++)
-			for(j = i - 1; j >= 0; j--)
-				if(IsCrash(obj[i], obj[j])
-					&& (Distance(obj[i].x, obj[i].y, obj[j].x, obj[j].y)
-					> Distance(obj[i].x + obj[i].vx, obj[i].y + obj[i].vy,
-					obj[j].x + obj[j].vx, obj[j].y + obj[j].vy)))
-					Crash(obj[i], obj[j]);
-	}
-
-	//根据属性值绘画
-	void
-	drawobj()
-	{
-		for(int i = 0; i < n; i++)
-		{
-			setfillcolor(obj[i].color);
-			ege_fillellipse(obj[i].x - obj[i].r, obj[i].y - obj[i].r,
-				obj[i].r * 2, obj[i].r * 2);
-		}
-	}
-
-private:
-	static bool
-	IsCrash(const Obj& a, const Obj& b)
-	{
-		return Distance(a.x, a.y, b.x, b.y) < (a.r + b.r) * (a.r + b.r);
-	}
-
-	static float
-	Distance(float x1, float y1, float x2, float y2)
-	{
-		return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+				a.init_xyr();
+			}while(std::any_of(&objs[0], &a, [&a, this](Obj& b){
+				return crash(a, b);
+			}));
 	}
 
 	void
-	Crash(Obj& a, Obj& b)
+	update()
 	{
-		float ma(a.r * a.r), mb(b.r * b.r), sx(a.x - b.x), sy(a.y - b.y),
-			s1x(sx / sqrt(sx * sx + sy * sy)),
-			s1y(sy / sqrt(sx * sx + sy * sy)),
-			t1x(-s1y), t1y(s1x), vas(a.vx * s1x + a.vy * s1y),
-			vat(a.vx * t1x + a.vy * t1y), vbs(b.vx * s1x + b.vy * s1y),
-			vbt(b.vx * t1x + b.vy * t1y),
-			vasf((2 * mb * vbs + vas * (ma - mb)) / (ma + mb)),
-			vbsf((2 * ma * vas - vbs * (ma - mb)) / (ma + mb)), nsx(vasf * s1x),
-			nsy(vasf * s1y), ntx(vat * t1x), nty(vat * t1y);
-
-		a.vx = nsx + ntx;
-		a.vy = nsy + nty;
-		nsx = vbsf * s1x;
-		nsy = vbsf * s1y;
-		ntx = vbt * t1x;
-		nty = vbt * t1y;
-		b.vx = nsx + ntx;
-		b.vy = nsy + nty;
+		for(auto& o : objs)
+			o.update();
+		for(auto& a : objs)
+			std::for_each(&objs[0], &a, [&a, this](Obj& b){
+				if(crash(a, b) && (dist(a.x, a.y, b.x, b.y)
+					> dist(a.x + a.vx, a.y + a.vy, b.x + b.vx, b.y + b.vy)))
+					check_crash(a, b);
+			});
+		cleardevice();
+		for(auto& o : objs)
+			o.draw();
 	}
 };
 
@@ -134,16 +131,12 @@ main()
 	setinitmode(INIT_ANIMATION);
 	initgraph(640, 480);
 
-	AniObj aniobj; //定义对象
+	AniObj ani(8);
 	fps f;
 
 	ege_enable_aa(true);
 	for(; is_run(); delay_fps(120))
-	{
-		aniobj.updateobj(); //更新位置
-		cleardevice();
-		aniobj.drawobj(); //绘画
-	}
+		ani.update();
 	closegraph();
 }
 
