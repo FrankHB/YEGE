14e37b60d74faf604f338da87c1fd5efe48f87aa
FrankHB <frankhb1989@gmail.com>
2015-04-07 20:37:32 +0800

    EGE/src/ege/: 修复未定义 UNICODE 宏时调用非 Unicode 默认窗口过程导致窗口标题设置编码错误等。
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- EGE/src/ege/global.cpp
+++ EGE/src/ege/global.cpp
@@ -127,7 +127,7 @@ wndproc(::HWND hWnd, unsigned message, ::WPARAM wParam, ::LPARAM lParam)
 		::SetFocus(::HWND(lParam));
 		break;
 	default:
-		return ::DefWindowProc(hWnd, message, wParam, lParam);
+		return ::DefWindowProcW(hWnd, message, wParam, lParam);
 	}
 	return 0;
 }
