e07677c76c1d9710ba9a92108fc2e3e9393312d0
FrankHB <frankhb1989@gmail.com>
2015-04-08 00:10:25 +0800

    EGE/: 提供和 YSLib 兼容的宏 YAssert（替换为 yassume 表达式）、DefNeg 、DefBoolNeg 和 DefSetter ；提供 YSLib 兼容的 min 和 max 模板（非 YSLib 使用 std 的对应名称代替，不保证 constexpr ）；提供 YSLib 兼容的 FetchZero 、HalfDifference 、IsInInterval 和 IsInOpenInterval 模板；提供和 YSLib::Drawing 兼容的模板 GBinaryGroup 、类型 Point 、Vec 、Size 和 Rect 以及相关操作。
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- EGE/include/ege/base.h
+++ EGE/include/ege/base.h
@@ -5,7 +5,10 @@
 #if !YEGE_Use_YSLib
 #	include <cstddef>
 #	include <cstdint>
+#	include <type_traits>
 #	include <memory>
+#	include <limits>
+#	include <tuple>
 #endif
 
 namespace ege
@@ -32,6 +35,20 @@ using platform::Deref;
 
 using YSLib::SPos;
 using YSLib::SDst;
+
+using YSLib::min;
+using YSLib::max;
+
+using YSLib::FetchZero;
+using YSLib::HalfDifference;
+using YSLib::IsInInterval;
+using YSLib::IsInOpenInterval;
+
+using YSLib::Drawing::GBinaryGroup;
+using YSLib::Drawing::Point;
+using YSLib::Drawing::Vec;
+using YSLib::Drawing::Size;
+using YSLib::Drawing::Rect;
 #else
 
 #define PDefH(_t, _n, ...) \
@@ -127,6 +144,14 @@ using YSLib::SDst;
 	operator _t() _q \
 	ImplRet(__VA_ARGS__)
 
+#define DefNeg \
+	PDefHOp(bool, !, ) const ynothrow \
+		ImplRet(!bool(*this))
+
+#define DefBoolNeg(_spec, ...) \
+	DefNeg \
+	_spec DefCvt(const ynothrow, bool, __VA_ARGS__)
+
 #define DefPred(_q, _n, ...) \
 	bool \
 	YPP_Concat(Is, _n)() _q \
@@ -136,6 +161,11 @@ using YSLib::SDst;
 	_t \
 	YPP_Concat(Get, _n)() _q \
 		ImplRet(__VA_ARGS__)
+
+#define DefSetter(_t, _n, _m) \
+	void \
+	YPP_Concat(Set, _n)(_t _tempArgName) \
+		ImplExpr((_m) = _tempArgName)
 
 #define DefSwap(_q, _t) \
 	PDefH(void, swap, _t& _x, _t& _y) _q \
@@ -192,8 +222,470 @@ Deref(_type&& p) -> decltype(*p)
 using SPos = long;
 using SDst = unsigned long;
 
+#define YAssert(_expr, _msg) yassume(_expr)
 #define YTraceDe(_lv, ...)
 
+using std::min;
+using std::max;
+
+template<typename _type>
+yconstfn _type
+FetchZero() ynothrow
+{
+	return _type(0);
+}
+
+template<typename _type>
+yconstfn _type
+HalfDifference(_type x, _type y)
+{
+	return (x - y) / 2;
+}
+
+template<typename _type>
+inline bool
+IsInInterval(_type i, _type b) ynothrow
+{
+	YAssert(FetchZero<_type>() < b,
+		"Zero element as lower bound is not less than upper bound.");
+	return !(i < FetchZero<_type>()) && i < b;
+}
+template<typename _type>
+inline bool
+IsInInterval(_type i, _type a, _type b) ynothrow
+{
+	YAssert(a < b, "Lower bound is not less than upper bound.");
+	return !(i < a) && i < b;
+}
+
+template<typename _type>
+inline bool
+IsInOpenInterval(_type i, _type b) ynothrow
+{
+	YAssert(FetchZero<_type>() < b,
+		"Zero element as lower bound is not less than upper bound.");
+	return FetchZero<_type>() < i && i < b;
+}
+template<typename _type>
+inline bool
+IsInOpenInterval(_type i, _type a, _type b) ynothrow
+{
+	YAssert(a < b,
+		"Lower bound is not less than upper bound.");
+	return a < i && i < b;
+}
+
+class Size;
+class Rect;
+
+template<typename _type>
+class GBinaryGroup
+{
+public:
+	static const GBinaryGroup Invalid;
+
+	_type X = 0, Y = 0;
+
+	yconstfn DefDeCtor(GBinaryGroup)
+	yconstfn DefDeCopyCtor(GBinaryGroup)
+	explicit yconstfn
+	GBinaryGroup(const Size&) ynothrow;
+	explicit yconstfn
+	GBinaryGroup(const Rect&) ynothrow;
+	template<typename _tScalar1, typename _tScalar2>
+	yconstfn
+	GBinaryGroup(_tScalar1 x, _tScalar2 y) ynothrow
+		: X(_type(x)), Y(_type(y))
+	{}
+	template<typename _tPair>
+	yconstfn
+	GBinaryGroup(const _tPair& pr) ynothrow
+		: X(std::get<0>(pr)), Y(std::get<1>(pr))
+	{}
+
+	DefDeCopyAssignment(GBinaryGroup)
+
+	yconstfn PDefHOp(GBinaryGroup, -, ) const ynothrow
+		ImplRet(GBinaryGroup(-X, -Y))
+
+	PDefHOp(GBinaryGroup&, +=, const GBinaryGroup& val) ynothrow
+		ImplRet(yunseq(X += val.X, Y += val.Y), *this)
+	PDefHOp(GBinaryGroup&, -=, const GBinaryGroup& val) ynothrow
+		ImplRet(yunseq(X -= val.X, Y -= val.Y), *this)
+
+	yconstfn DefGetter(const ynothrow, _type, X, X)
+	yconstfn DefGetter(const ynothrow, _type, Y, Y)
+
+	DefSetter(_type, X, X)
+	DefSetter(_type, Y, Y)
+
+	yconstfn DefPred(const ynothrow, Zero, X == 0 && Y == 0)
+
+	PDefH(_type&, GetRef, bool b = true) ynothrow
+		ImplRet(b ? X : Y)
+	PDefH(const _type&, GetRef, bool b = true) const ynothrow
+		ImplRet(b ? X : Y)
+};
+
+template<typename _type>
+const GBinaryGroup<_type> GBinaryGroup<_type>::Invalid{
+	std::numeric_limits<_type>::lowest(), std::numeric_limits<_type>::lowest()};
+
+template<typename _type>
+yconstfn bool
+operator==(const GBinaryGroup<_type>& x, const GBinaryGroup<_type>& y) ynothrow
+{
+	return x.X == y.X && x.Y == y.Y;
+}
+
+template<typename _type>
+yconstfn bool
+operator!=(const GBinaryGroup<_type>& x, const GBinaryGroup<_type>& y) ynothrow
+{
+	return !(x == y);
+}
+
+template<typename _type>
+yconstfn GBinaryGroup<_type>
+operator+(const GBinaryGroup<_type>& x, const GBinaryGroup<_type>& y) ynothrow
+{
+	return GBinaryGroup<_type>(x.X + y.X, x.Y + y.Y);
+}
+
+template<typename _type>
+yconstfn GBinaryGroup<_type>
+operator-(const GBinaryGroup<_type>& x, const GBinaryGroup<_type>& y) ynothrow
+{
+	return GBinaryGroup<_type>(x.X - y.X, x.Y - y.Y);
+}
+
+template<typename _type, typename _tScalar>
+yconstfn GBinaryGroup<_type>
+operator*(const GBinaryGroup<_type>& val, _tScalar l) ynothrow
+{
+	return GBinaryGroup<_type>(val.X * l, val.Y * l);
+}
+
+template<class _tBinary>
+yconstfn _tBinary
+Transpose(const _tBinary& obj) ynothrow
+{
+	return _tBinary(obj.Y, obj.X);
+}
+
+template<typename _type>
+yconstfn GBinaryGroup<_type>
+TransposeCCW(const GBinaryGroup<_type>& val) ynothrow
+{
+	return GBinaryGroup<_type>(val.Y, -val.X);
+}
+
+template<typename _type>
+yconstfn GBinaryGroup<_type>
+TransposeCW(const GBinaryGroup<_type>& val) ynothrow
+{
+	return GBinaryGroup<_type>(-val.Y, val.X);
+}
+
+template<size_t _vIdx, typename _type>
+yconstfn _type&
+get(GBinaryGroup<_type>& val)
+{
+	static_assert(_vIdx < 2, "Invalid index found.");
+
+	return _vIdx == 0 ? val.X : val.Y;
+}
+template<size_t _vIdx, typename _type>
+yconstfn const _type&
+get(const GBinaryGroup<_type>& val)
+{
+	static_assert(_vIdx < 2, "Invalid index found.");
+
+	return _vIdx == 0 ? val.X : val.Y;
+}
+
+
+using Point = GBinaryGroup<SPos>;
+
+using Vec = GBinaryGroup<SPos>;
+
+
+class EGEAPI Size
+{
+public:
+	static const Size Invalid;
+
+	SDst Width, Height; //!< 宽和高。
+
+	yconstfn
+	Size() ynothrow
+		: Width(0), Height(0)
+	{}
+	yconstfn
+	Size(const Size& s) ynothrow
+		: Width(s.Width), Height(s.Height)
+	{}
+	explicit yconstfn
+	Size(const Rect&) ynothrow;
+	template<typename _type>
+	explicit yconstfn
+	Size(const GBinaryGroup<_type>& val) ynothrow
+		: Width(static_cast<SDst>(val.X)), Height(static_cast<SDst>(val.Y))
+	{}
+	template<typename _tScalar1, typename _tScalar2>
+	yconstfn
+	Size(_tScalar1 w, _tScalar2 h) ynothrow
+		: Width(static_cast<SDst>(w)), Height(static_cast<SDst>(h))
+	{}
+
+	DefDeCopyAssignment(Size)
+
+	yconstfn DefBoolNeg(yconstfn explicit, Width != 0 || Height != 0)
+
+	PDefHOp(Size&, &=, const Size& s) ynothrow
+		ImplRet(yunseq(Width = min(Width, s.Width),
+			Height = min(Height, s.Height)), *this)
+
+	PDefHOp(Size&, |=, const Size& s) ynothrow
+		ImplRet(yunseq(Width = max(Width, s.Width),
+			Height = max(Height, s.Height)), *this)
+
+	yconstfn DefCvt(const ynothrow, Vec, Vec(Width, Height))
+
+	yconstfn DefPred(const ynothrow, LineSegment,
+		!((Width == 0) ^ (Height == 0)))
+	yconstfn DefPred(const ynothrow, UnstrictlyEmpty, Width == 0 || Height == 0)
+
+	PDefH(SDst&, GetRef, bool b = true) ynothrow
+		ImplRet(b ? Width : Height)
+	PDefH(const SDst&, GetRef, bool b = true) const ynothrow
+		ImplRet(b ? Width : Height)
+};
+
+yconstfn PDefHOp(bool, ==, const Size& x, const Size& y) ynothrow
+	ImplRet(x.Width == y.Width && x.Height == y.Height)
+
+yconstfn PDefHOp(bool, !=, const Size& x, const Size& y) ynothrow
+	ImplRet(!(x == y))
+
+inline PDefHOp(Size, &, const Size& x, const Size& y) ynothrow
+	ImplRet({min(x.Width, y.Width), min(x.Height, y.Height)})
+
+inline PDefHOp(Size, |, const Size& x, const Size& y) ynothrow
+	ImplRet({max(x.Width, y.Width), max(x.Height, y.Height)})
+
+yconstfn PDefH(auto, GetAreaOf, const Size& s) ynothrow
+	-> decltype(s.Width * s.Height)
+	ImplRet(s.Width * s.Height)
+
+yconstfn PDefH(Point, LocateCenter, const Size& x, const Size& y)
+	ImplRet({HalfDifference(SPos(x.Width), SPos(y.Width)),
+		HalfDifference(SPos(x.Height), SPos(y.Height))})
+
+yconstfn PDefH(Point, RotateCenter, const Size& s)
+	ImplRet({HalfDifference(SPos(s.Width), SPos(s.Height)),
+		HalfDifference(SPos(s.Height), SPos(s.Width))})
+
+template<size_t _vIdx>
+yconstfn SDst&
+get(Size& s)
+{
+	static_assert(_vIdx < 2, "Invalid index found.");
+
+	return _vIdx == 0 ? s.Width : s.Height;
+}
+template<size_t _vIdx>
+yconstfn const SDst&
+get(const Size& s)
+{
+	static_assert(_vIdx < 2, "Invalid index found.");
+
+	return _vIdx == 0 ? s.Width : s.Height;
+}
+
+
+template<typename _type>
+yconstfn GBinaryGroup<_type>
+operator+(GBinaryGroup<_type> val, const Size& s) ynothrow
+{
+	return {val.X + _type(s.Width), val.Y + _type(s.Height)};
+}
+
+template<typename _type>
+yconstfn GBinaryGroup<_type>
+operator-(GBinaryGroup<_type> val, const Size& s) ynothrow
+{
+	return {val.X - _type(s.Width), val.Y - _type(s.Height)};
+}
+
+
+yconstfn PDefH(Size, Transpose, const Size& s) ynothrow
+	ImplRet({s.Height, s.Width})
+
+
+template<typename _tScalar = float>
+inline _tScalar
+ScaleMin(const Size& x, const Size& y, _tScalar threshold = 1.F)
+{
+	return std::min({threshold,
+		_tScalar(_tScalar(x.Width) / _tScalar(y.Width)),
+		_tScalar(_tScalar(x.Height) / _tScalar(y.Height))});
+}
+
+
+class EGEAPI Rect : private Point, private Size
+{
+public:
+	static const Rect Invalid;
+
+	using Point::X;
+	using Point::Y;
+	using Size::Width;
+	using Size::Height;
+
+	DefDeCtor(Rect)
+	explicit yconstfn
+	Rect(const Point& pt) ynothrow
+		: Point(pt), Size()
+	{}
+	yconstfn
+	Rect(const Size& s) ynothrow
+		: Point(), Size(s)
+	{}
+	yconstfn
+	Rect(const Point& pt, const Size& s) ynothrow
+		: Point(pt), Size(s)
+	{}
+	yconstfn
+	Rect(const Point& pt, SDst w, SDst h) ynothrow
+		: Point(pt.X, pt.Y), Size(w, h)
+	{}
+	yconstfn
+	Rect(SPos x, SPos y, const Size& s) ynothrow
+		: Point(x, y), Size(s.Width, s.Height)
+	{}
+	yconstfn
+	Rect(SPos x, SPos y, SDst w, SDst h) ynothrow
+		: Point(x, y), Size(w, h)
+	{}
+	yconstfn DefDeCopyCtor(Rect)
+
+	DefDeCopyAssignment(Rect)
+	Rect&
+	operator=(const Point& pt) ynothrow
+	{
+		yunseq(X = pt.X, Y = pt.Y);
+		return *this;
+	}
+	Rect&
+	operator=(const Size& s) ynothrow
+	{
+		yunseq(Width = s.Width, Height = s.Height);
+		return *this;
+	}
+
+	Rect&
+	operator&=(const Rect&) ynothrow;
+
+	Rect&
+	operator|=(const Rect&) ynothrow;
+
+	using Size::operator!;
+
+	using Size::operator bool;
+
+	bool
+	Contains(SPos px, SPos py) const ynothrow;
+	PDefH(bool, Contains, const Point& pt) const ynothrow
+		ImplRet(Contains(pt.X, pt.Y))
+	bool
+	Contains(const Rect&) const ynothrow;
+	bool
+	ContainsStrict(SPos px, SPos py) const ynothrow;
+	PDefH(bool, ContainsStrict, const Point& pt) const ynothrow
+		ImplRet(ContainsStrict(pt.X, pt.Y))
+	bool
+	ContainsStrict(const Rect&) const ynothrow;
+	using Size::IsLineSegment;
+	using Size::IsUnstrictlyEmpty;
+
+	yconstfn DefGetter(const ynothrow, SPos, Bottom, Y + SPos(Height))
+	yconstfn DefGetter(const ynothrow, const Point&, Point,
+		static_cast<const Point&>(*this))
+	DefGetter(ynothrow, Point&, PointRef, static_cast<Point&>(*this))
+	yconstfn DefGetter(const ynothrow, SPos, Right, X + SPos(Width))
+	yconstfn DefGetter(const ynothrow, const Size&, Size,
+		static_cast<const Size&>(*this))
+	DefGetter(ynothrow, Size&, SizeRef, static_cast<Size&>(*this))
+	using Point::GetX;
+	using Point::GetY;
+
+	using Point::SetX;
+	using Point::SetY;
+};
+
+yconstfn PDefHOp(bool, ==, const Rect& x, const Rect& y) ynothrow
+	ImplRet(x.GetPoint() == y.GetPoint() && x.GetSize() == y.GetSize())
+
+yconstfn PDefHOp(bool, !=, const Rect& x, const Rect& y) ynothrow
+	ImplRet(!(x == y))
+
+yconstfn PDefHOp(Rect, +, const Rect& r, const Vec& v) ynothrow
+	ImplRet({r.GetPoint() + v, r.GetSize()})
+
+yconstfn PDefHOp(Rect, -, const Rect& r, const Vec& v) ynothrow
+	ImplRet({r.GetPoint() - v, r.GetSize()})
+
+inline PDefHOp(Rect, &, const Rect& x, const Rect& y) ynothrow
+	ImplRet(Rect(x) &= y)
+
+inline PDefHOp(Rect, |, const Rect& x, const Rect& y) ynothrow
+	ImplRet(Rect(x) |= y)
+
+inline PDefH(void, Diminish, Rect& r, SDst off1 = 1, SDst off2 = 2)
+	ImplExpr(YAssert(r.Width > off2 && r.Height > off2,
+		"Boundary is too small."), yunseq(r.X += SPos(off1), r.Y += SPos(off1),
+		r.Width -= off2, r.Height -= off2))
+
+inline PDefH(Rect, MakeRect, const Point& pt1, const Point& pt2) ynothrow
+	ImplRet(Rect(std::min(pt1.X, pt2.X), std::min(pt1.Y, pt2.Y),
+		SDst(pt1.X < pt2.X ? pt2.X - pt1.X : pt1.X - pt2.X),
+		SDst(pt1.Y < pt2.Y ? pt2.Y - pt1.Y : pt1.Y - pt2.Y)))
+
+inline PDefH(Rect, TransposeCCW, const Rect& r) ynothrow
+	ImplRet(MakeRect(TransposeCCW(Point(r.GetRight(), r.Y)),
+		TransposeCCW(Point(r.X, r.GetBottom()))))
+
+inline PDefH(Rect, TransposeCW, const Rect& r) ynothrow
+	ImplRet(MakeRect(TransposeCW(Point(r.GetRight(), r.Y)),
+		TransposeCW(Point(r.X, r.GetBottom()))))
+
+template<size_t _vIdx>
+yconstfn typename std::conditional<_vIdx < 2, SPos, SDst>::type&
+get(Rect& r)
+{
+	return std::get<_vIdx>(std::tuple<SPos&, SPos&, SDst&, SDst&>(r.X, r.Y,
+		r.Width, r.Height));
+}
+template<size_t _vIdx>
+yconstfn const typename std::conditional<_vIdx < 2, SPos, SDst>::type&
+get(const Rect& r)
+{
+	return std::get<_vIdx>(std::tuple<const SPos&, const SPos&, const SDst&,
+		const SDst&>(r.X, r.Y, r.Width, r.Height));
+}
+
+
+template<typename _type>
+yconstfn
+GBinaryGroup<_type>::GBinaryGroup(const Rect& r) ynothrow
+	: X(r.X), Y(r.Y)
+{}
+
+yconstfn
+Size::Size(const Rect& r) ynothrow
+	: Width(r.Width), Height(r.Height)
+{}
 #endif
 
 const double PI = 3.14159265358979323;
--- EGE/src/ege/base.cpp
+++ EGE/src/ege/base.cpp
@@ -1,5 +1,100 @@
-﻿namespace ege
+﻿#include "ege/base.h"
+
+namespace ege
 {
 
+#if !YEGE_Use_YSLib
+
+const Size Size::Invalid(std::numeric_limits<SDst>::lowest(),
+	std::numeric_limits<SDst>::lowest());
+
+namespace
+{
+
+bool
+RectContainsRaw(const Rect& r, SPos px, SPos py) ynothrow
+{
+	YAssert(r.Width > 0, "Invalid width found."),
+	YAssert(r.Height > 0, "Invalid height found.");
+
+	return IsInInterval<SPos>(px - r.X, SPos(r.Width))
+		&& IsInInterval<SPos>(py - r.Y, SPos(r.Height));
 }
+inline bool
+RectContainsRaw(const Rect& r, const Point& pt) ynothrow
+{
+	return RectContainsRaw(r, pt.X, pt.Y);
+}
+
+bool
+RectContainsStrictRaw(const Rect& r, SPos px, SPos py) ynothrow
+{
+	YAssert(r.Width > 1, "Invalid width found."),
+	YAssert(r.Height > 1, "Invalid height found.");
+	// XXX: Conversion to 'SPos' might be implementation-defined.
+	return IsInOpenInterval<SPos>(px - r.X, SPos(r.Width - 1))
+		&& IsInOpenInterval<SPos>(py - r.Y, SPos(r.Height - 1));
+}
+inline bool
+RectContainsStrictRaw(const Rect& r, const Point& pt) ynothrow
+{
+	return RectContainsStrictRaw(r, pt.X, pt.Y);
+}
+
+} // unnamed namespace;
+
+const Rect Rect::Invalid(Size::Invalid);
+
+bool
+Rect::Contains(SPos px, SPos py) const ynothrow
+{
+	return !IsUnstrictlyEmpty() && RectContainsRaw(*this, px, py);
+}
+bool
+Rect::Contains(const Rect& r) const ynothrow
+{
+	return !IsUnstrictlyEmpty() && RectContainsRaw(*this, r.GetPoint())
+		&& RectContainsRaw(*this, r.GetPoint() + r.GetSize() - Vec(1, 1));
+}
+
+bool
+Rect::ContainsStrict(SPos px, SPos py) const ynothrow
+{
+	return Width > 1 && Height > 1 && RectContainsStrictRaw(*this, px, py);
+}
+bool
+Rect::ContainsStrict(const Rect& r) const ynothrow
+{
+	return Width > 1 && Height > 1 && !r.IsUnstrictlyEmpty()
+		&& RectContainsStrictRaw(*this, r.GetPoint())
+		&& RectContainsStrictRaw(*this, r.GetPoint() + r.GetSize() - Vec(1, 1));
+}
+
+Rect&
+Rect::operator&=(const Rect& r) ynothrow
+{
+	const SPos x1(max(X, r.X)), x2(min(GetRight(), r.GetRight())),
+		y1(max(Y, r.Y)), y2(min(GetBottom(), r.GetBottom()));
+
+	return *this = x2 < x1 || y2 < y1 ? Rect()
+		: Rect(x1, y1, SDst(x2 - x1), SDst(y2 - y1));
+}
+
+Rect&
+Rect::operator|=(const Rect& r) ynothrow
+{
+	if(!*this)
+		return *this = r;
+	if(!r)
+		return *this;
+
+	const SPos mx(min(X, r.X)), my(min(Y, r.Y));
+
+	return *this = Rect(mx, my, SDst(max(GetRight(), r.GetRight())
+		- mx), SDst(max(GetBottom(), r.GetBottom()) - my));
+}
+
+#endif
+
+} // namespace ege;
 
