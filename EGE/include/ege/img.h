2e72cde6fe8dd0ef4a476f5b585186195068a428
FrankHB <frankhb1989@gmail.com>
2020-07-26 12:22:50 +0800

    EGE/include/ege/img.h: 调整注释格式。
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- EGE/include/ege/img.h
+++ EGE/include/ege/img.h
@@ -51,59 +51,59 @@ getimage(IMAGE* pDstImg, const IMAGE* pSrcImg, int srcX, int srcY, int srcWidth,
 
 EGEAPI int
 putimage_transparent(
-	IMAGE* imgdest,         //handle to dest
-	const IMAGE* imgsrc,          //handle to source
-	int nXOriginDest,       //x-coord of destination upper-left corner
-	int nYOriginDest,       //y-coord of destination upper-left corner
-	color_t crTransparent,  //color to make transparent
-	int nXOriginSrc = 0,    //x-coord of source upper-left corner
-	int nYOriginSrc = 0,    //y-coord of source upper-left corner
-	int nWidthSrc = 0,      //width of source rectangle
-	int nHeightSrc = 0      //height of source rectangle
+	IMAGE* imgdest,         // handle to dest
+	const IMAGE* imgsrc,    // handle to source
+	int nXOriginDest,       // x-coord of destination upper-left corner
+	int nYOriginDest,       // y-coord of destination upper-left corner
+	color_t crTransparent,  // color to make transparent
+	int nXOriginSrc = 0,    // x-coord of source upper-left corner
+	int nYOriginSrc = 0,    // y-coord of source upper-left corner
+	int nWidthSrc = 0,      // width of source rectangle
+	int nHeightSrc = 0      // height of source rectangle
 );
 
 EGEAPI int
 putimage_alphablend(
-	IMAGE* imgdest,         //handle to dest
-	const IMAGE* imgsrc,          //handle to source
-	int nXOriginDest,       //x-coord of destination upper-left corner
-	int nYOriginDest,       //y-coord of destination upper-left corner
-	unsigned char alpha,    //alpha
-	int nXOriginSrc = 0,    //x-coord of source upper-left corner
-	int nYOriginSrc = 0,    //y-coord of source upper-left corner
-	int nWidthSrc = 0,      //width of source rectangle
-	int nHeightSrc = 0      //height of source rectangle
+	IMAGE* imgdest,         // handle to dest
+	const IMAGE* imgsrc,    // handle to source
+	int nXOriginDest,       // x-coord of destination upper-left corner
+	int nYOriginDest,       // y-coord of destination upper-left corner
+	unsigned char alpha,    // alpha
+	int nXOriginSrc = 0,    // x-coord of source upper-left corner
+	int nYOriginSrc = 0,    // y-coord of source upper-left corner
+	int nWidthSrc = 0,      // width of source rectangle
+	int nHeightSrc = 0      // height of source rectangle
 );
 
 EGEAPI int
 putimage_alphatransparent(
-	IMAGE* imgdest,         //handle to dest
-	const IMAGE* imgsrc,          //handle to source
-	int nXOriginDest,       //x-coord of destination upper-left corner
-	int nYOriginDest,       //y-coord of destination upper-left corner
-	color_t crTransparent,  //color to make transparent
-	unsigned char alpha,    //alpha
-	int nXOriginSrc = 0,    //x-coord of source upper-left corner
-	int nYOriginSrc = 0,    //y-coord of source upper-left corner
-	int nWidthSrc = 0,      //width of source rectangle
-	int nHeightSrc = 0      //height of source rectangle
+	IMAGE* imgdest,         // handle to dest
+	const IMAGE* imgsrc,    // handle to source
+	int nXOriginDest,       // x-coord of destination upper-left corner
+	int nYOriginDest,       // y-coord of destination upper-left corner
+	color_t crTransparent,  // color to make transparent
+	unsigned char alpha,    // alpha
+	int nXOriginSrc = 0,    // x-coord of source upper-left corner
+	int nYOriginSrc = 0,    // y-coord of source upper-left corner
+	int nWidthSrc = 0,      // width of source rectangle
+	int nHeightSrc = 0      // height of source rectangle
 );
 
 EGEAPI int
 putimage_withalpha(
-	IMAGE* imgdest,         //handle to dest
-	const IMAGE* imgsrc,          //handle to source
-	int nXOriginDest,       //x-coord of destination upper-left corner
-	int nYOriginDest,       //y-coord of destination upper-left corner
-	int nXOriginSrc = 0,    //x-coord of source upper-left corner
-	int nYOriginSrc = 0,    //y-coord of source upper-left corner
-	int nWidthSrc = 0,      //width of source rectangle
-	int nHeightSrc = 0      //height of source rectangle
+	IMAGE* imgdest,         // handle to dest
+	const IMAGE* imgsrc,    // handle to source
+	int nXOriginDest,       // x-coord of destination upper-left corner
+	int nYOriginDest,       // y-coord of destination upper-left corner
+	int nXOriginSrc = 0,    // x-coord of source upper-left corner
+	int nYOriginSrc = 0,    // y-coord of source upper-left corner
+	int nWidthSrc = 0,      // width of source rectangle
+	int nHeightSrc = 0      // height of source rectangle
 );
 
 EGEAPI int
 imagefilter_blurring(
-	IMAGE* imgdest,         //handle to dest
+	IMAGE* imgdest,         // handle to dest
 	int intensity,
 	int alpha,
 	int nXOriginDest = 0,
@@ -121,8 +121,8 @@ putimage_rotate(
 	float centerx,
 	float centery,
 	float radian,
-	int btransparent = 0, //transparent (1) or not (0)
-	int alpha = -1, //in range[0, 256], alpha== -1 means no alpha
+	int btransparent = 0, // transparent (1) or not (0)
+	int alpha = -1, // in range[0, 256], alpha== -1 means no alpha
 	int smooth = 0
 );
 
