c8666d04d2e29a703891d5974577f6fce5bc12b3
FrankHB <frankhb1989@gmail.com>
2024-01-21 21:51:24 +0800

    EGE/src/ege/windows.cpp: 简化内部函数模板名称（和当前 YSLib 中的名称相同）。
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- EGE/src/ege/windows.cpp
+++ EGE/src/ege/windows.cpp
@@ -1,4 +1,4 @@
-﻿#include "ege/windows.h"
+﻿#include "ege/windows.h" // for ::WideCharToMultiByte;
 #include "global.h"
 #if !YEGE_Use_YSLib
 #	include <limits>
@@ -15,7 +15,7 @@ namespace
 
 template<typename _tDst, typename _type>
 inline _tDst
-CheckScalar(_type val, const std::string& = "")
+CheckUpperBound(_type val, const std::string& = "")
 {
 	using common_t = typename std::common_type<_tDst, _type>::type;
 
@@ -26,11 +26,11 @@ CheckScalar(_type val, const std::string& = "")
 
 template<typename _tDst, typename _type>
 inline _tDst
-CheckPositiveScalar(_type val, const std::string& name = "")
+CheckPositive(_type val, const std::string& name = "")
 {
 	if(!(0 < val))
 		throw std::runtime_error("failed getting positive value");
-	return CheckScalar<_tDst>(val, name);
+	return CheckUpperBound<_tDst>(val, name);
 }
 
 using Pixel = color_int_t*;
@@ -39,8 +39,8 @@ using Pixel = color_int_t*;
 
 ScreenBuffer::ScreenBuffer(const Size& s)
 	: size(s), hBitmap([this]{
-		::BITMAPINFO bmi{{sizeof(::BITMAPINFOHEADER), CheckPositiveScalar<SPos>(
-			size.Width, "width"), -CheckPositiveScalar<SPos>(size.Height,
+		::BITMAPINFO bmi{{sizeof(::BITMAPINFOHEADER), CheckPositive<SPos>(
+			size.Width, "width"), -CheckPositive<SPos>(size.Height,
 			"height") - 1, 1, 32, BI_RGB, static_cast<unsigned long>(
 			sizeof(Pixel) * size.Width * size.Height), 0, 0, 0, 0}, {}};
 		void* p_buf{};
@@ -113,6 +113,13 @@ getHWnd()
 getHInstance()
 {
 	return EGEApplication::GetInstance();
+}
+
+
+std::string
+WCSToMBCS(const wchar_t* str, unsigned cp)
+{
+	return WCSToMBCSImpl<std::string>({}, -1, str, cp);
 }
 
 } // namespace ege;
